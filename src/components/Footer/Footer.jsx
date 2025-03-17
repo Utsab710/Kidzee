a87326d0bc6cc7844013d1cae765a26737b94709
Your Name <Utsab710>
2025-03-18 00:54:50 +0545

    Changes in footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Form from "../Form/Form";
 import kidzee from "../../image/kidzee.png";
+import "../Custom_css/AnimateCloud.css";
 
 function Footer() {
   return (
     <div className="w-full bg-cyan-100">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col lg:flex-row py-8">
-          <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
+        {/* Clouds positioned correctly */}
+        <div className="relative">
+          <img
+            src="https://kidzeeboudha.com/assets/site/img/value/cloud-1.png"
+            alt="Floating cloud"
+            className="w-auto h-auto floating-cloud absolute top-0 right-0 p-4"
+          />
+          <img
+            src="https://kidzeeboudha.com/assets/site/img/value/cloud-2.png"
+            alt="Floating cloud"
+            className="w-auto h-auto floating-cloud absolute top-0 right-1/6 p-8"
+          />
+        </div>
+
+        <div className="flex flex-col lg:flex-row py-8 mt-16">
+          <div className="w-full lg:w-1/2 mb-8 lg:mb-6">
             <Form />
           </div>
           <div className="w-full lg:w-1/2 flex items-end justify-center lg:justify-end relative">
@@ -27,7 +42,7 @@ function Footer() {
           <img
             src="https://kidzeeboudha.com/assets/site/img/footer-top.png"
             alt="Flower border"
-            className="w-full "
+            className="w-full"
           />
         </div>
       </div>
